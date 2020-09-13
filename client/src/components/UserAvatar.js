50046d43ef43ef14d2cf1e177a76ad0df6c4cf26
aless-ishy <alessandro.ishy.medeiros@gmail.com>
2020-09-13 13:08:59 -0400

    sign-up form

--- client/src/components/UserAvatar.js
+++ client/src/components/UserAvatar.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import "../assets/stylesheets/User.scss";
+import addPhoto from "../assets/icons/add_photo.svg";
 
 class UserAvatar extends Component {
     render() {
@@ -7,6 +8,9 @@ class UserAvatar extends Component {
             <div className="user-container">
                 <div className="user-head"/>
                 <div className="user-body"/>
+                <div className="button-container">
+                    <img src={addPhoto} alt=""/>
+                </div>
             </div>
         );
     }
--- client/src/containers/SignUp.js
+++ client/src/containers/SignUp.js
@@ -1,15 +1,32 @@
 import React, {Component} from 'react';
 import "../assets/stylesheets/SignUp.scss";
 import UserAvatar from "../components/UserAvatar";
-import {TextField} from "@material-ui/core";
+import {Button, TextField} from "@material-ui/core";
 
 class SignUp extends Component {
     render() {
         return (
             <div className="sign-up-container">
+                <h1>Sign Up</h1>
                 <UserAvatar/>
                 <div className="sign-up-form">
-                    <TextField/>
+                    <div className="sign-up-form-name">
+                        <div className="field-small">
+                            <TextField fullWidth variant="outlined" label="Name" id="name"/>
+                        </div>
+                        <div className="field-small">
+                            <TextField fullWidth variant="outlined" label="Last Name" id="lastname"/>
+                        </div>
+                    </div>
+                    <div className="field-large">
+                        <TextField fullWidth variant="outlined" label="Email" id="email"/>
+                    </div>
+                    <div className="field-large">
+                        <TextField fullWidth variant="outlined" label="Password" id="password"/>
+                    </div>
+                    <div className="sign-up-form-action-buttons">
+                        <Button>SIGN UP</Button>
+                    </div>
                 </div>
             </div>
         );
